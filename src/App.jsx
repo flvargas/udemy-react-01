b01d33507843e090de3553cf760913a7457bc15a
flvargas <vargas.flavio@gmail.com>
2025-09-03 16:30:22 -0300

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import Header from "./Header";
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
+import { CORE_CONCEPTS } from './data.js';
 
 function App() {
   return (
-    <div>     
+    <div>      
       <Header /> 
       <main>
-        <h2>Time to get started!</h2>        
+        <section id="core-concepts">
+          <h2>Time to get started!</h2>        
+          <ul>            
+            <CoreConcept 
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />  
+            <CoreConcept {...CORE_CONCEPTS[1]} />  
+            <CoreConcept {...CORE_CONCEPTS[2]} />  
+            <CoreConcept {...CORE_CONCEPTS[3]} />                              
+          </ul>
+        </section>
       </main>
     </div>
   );
---
+++ src/components/CoreConcept/CoreConcept.jsx
@@ -0,0 +1,23 @@
+import './CoreConcept.css';
+
+function CoreConcept ({image, title, description}) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  )
+}
+// 2 ways same result
+function CoreConceptBACKUP (props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  )
+}
+
+export default CoreConcept;
