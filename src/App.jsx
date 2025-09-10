11d1aadde5f82d046bba0ba31f2c4e056fd36f08
flvargas <vargas.flavio@gmail.com>
2025-09-10 18:42:15 -0300

    lesson 57

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,30 @@ import { CORE_CONCEPTS, EXAMPLES } from './data.js';
 
 function App() {
 
-  const [ selectedTopic,setSelectedTopic ] = useState('components');
+  const [ selectedTopic,setSelectedTopic ] = useState();
 
   function handleSelect (selectedButton){
     setSelectedTopic(selectedButton);    
   }
 
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+      <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>
+            {EXAMPLES[selectedTopic].code}
+          </code>
+        </pre>
+      </div>
+    );
+  } 
+
   return (
-    <div>      
+    <div>    
       <Header /> 
       <main>
         <section id="core-concepts">
@@ -37,15 +53,7 @@ function App() {
             <TabButton onSelect={()=> handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={()=> handleSelect('state')}>State</TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-                {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>          
-          </div>
+          {tabContent}                            
         </section>        
       </main>
     </div>
